20f50d9b32102e24ecadb1aa526a3df801d5a83a
JavierPadronGarcia <padrongarciajavier04@gmail.com>
2023-04-22 13:03:38 +0100

    FIX twitter-component for a splide

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -2,6 +2,7 @@
 import Footer from "../../components/footer/Footer";
 import ImageCarrusel from "../../components/imageCarousel/ImageCarousel";
 import NavigationBar from "../../components/navbar/NavigationBar";
+import TwitterTweet from "../../components/twitterTweet/TwitterTweet";
 import "./Homepage.css";
 
 function Homepage() {
@@ -11,8 +12,10 @@ function Homepage() {
       {/* Header */}
       <NavigationBar />
       <ImageCarrusel />
-      <div className="empty-space">
-
+      <div className="relevant-news">
+        
+        <p>Noticias relevantes</p>
+        <TwitterTweet/>
       </div>
       <Footer />
     </div>
@@ -21,10 +24,13 @@ function Homepage() {
     <blockquote class="twitter-tweet">
     <p lang="es" dir="ltr">Un grupo de alumnos del centro educativo de San Cristóbal, en <a href="https://twitter.com/hashtag/LasPalmasdeGranCanaria?src=hash&amp;ref_src=twsrc%5Etfw">#LasPalmasdeGranCanaria</a>,
      ha puesto en marcha una ruta por el barrio para defender su patrimonio inmaterial
-     <br>
-     <br>Los primeros en disfrutar de este <a href="https://twitter.com/hashtag/turismo?src=hash&amp;ref_src=twsrc%5Etfw">#turismo</a> 
-     marinero piloto han sido estudiantes daneses de <a href="https://twitter.com/hashtag/Erasmus?src=hash&amp;ref_src=twsrc%5Etfw">#Erasmus
-     </a> <a href="https://t.co/VoGcrPO6I7">pic.twitter.com/VoGcrPO6I7</a></p>&mdash; RTVC (@RTVCes) 
+     <br/>
+     <br/>Los primeros en disfrutar de este 
+     <a href="https://twitter.com/hashtag/turismo?src=hash&amp;ref_src=twsrc%5Etfw">#turismo</a> 
+     marinero piloto han sido estudiantes daneses de 
+     <a href="https://twitter.com/hashtag/Erasmus?src=hash&amp;ref_src=twsrc%5Etfw">#Erasmus</a> 
+     <a href="https://t.co/VoGcrPO6I7">pic.twitter.com/VoGcrPO6I7</a>
+     </p>&mdash; RTVC (@RTVCes) 
      <a href="https://twitter.com/RTVCes/status/1583120355228426242?ref_src=twsrc%5Etfw">October 20, 2022</a>
      </blockquote> 
      <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"/>
